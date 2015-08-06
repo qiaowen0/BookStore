3f367fdd6153d4cf51fd97e58e8a6fd1b1ffc1a9
qiaowen0 <qiaowen0@live.com>
2015-08-06 14:55:56 +0800

    Profile completed!

--- src/com/bookstore/action/Profileaction.java
+++ src/com/bookstore/action/Profileaction.java
@@ -6,31 +6,19 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 
-public class SearchBookAuthor extends ActionSupport {
+public class Profileaction extends ActionSupport {
 	List<BookDetails> booklist = new ArrayList<BookDetails>();
-
 	public List<BookDetails> getBooklist() {
 		return booklist;
 	}
-
 	public void setBooklist(List<BookDetails> booklist) {
 		this.booklist = booklist;
 	}
-
-	private String bookauthor;
-
-	public String getBookauthor() {
-		return bookauthor;
-	}
-
-	public void setBookauthor(String bookauthor) {
-		this.bookauthor = bookauthor;
-	}
-
 	public String execute() throws Exception {
 		Connection con;
 		Statement stmt;
@@ -42,15 +30,15 @@ public class SearchBookAuthor extends ActionSupport {
 		String dbPwd = "12345678";
 		con = java.sql.DriverManager.getConnection(dbUrl, dbUser, dbPwd);
 		stmt = con.createStatement();
-		String sql = "SELECT count(*) FROM booktable where BookAuthor=\""
-				+ bookauthor + "\";";
+		ActionContext context = ActionContext.getContext();
+		Map<String, Object> session = (Map<String, Object>) context.getSession();
+		String recommenderuser=(String) session.get("username");	
+		String sql = "SELECT count(*) FROM booktable where RecommendUser=\""+ recommenderuser + "\";";
 		rs = stmt.executeQuery(sql);
 		rs.next();
 		String ColSum = rs.getString(1);
-		int colsum = Integer.parseInt(ColSum);
-
-		sql = "SELECT * FROM booktable where BookAuthor=\"" + bookauthor
-				+ "\";";
+		int colsum = Integer.parseInt(ColSum);		
+		sql = "SELECT BookId,Bookname FROM booktable where RecommendUser=\""+ recommenderuser + "\";";
 		rs = stmt.executeQuery(sql);
 		rs.next();
 		// 这里是否要先next？
@@ -58,12 +46,10 @@ public class SearchBookAuthor extends ActionSupport {
 			BookDetails bookdetails = new BookDetails();
 			bookdetails.setBookid(rs.getInt(1));
 			bookdetails.setBookname(rs.getString(2));
-			bookdetails.setBookauthor(rs.getString(3));
-			bookdetails.setRecommenduser(rs.getString(4));
 			rs.next();
 			booklist.add(bookdetails);
 		}
 		return SUCCESS;
+		
 	}
-
 }
